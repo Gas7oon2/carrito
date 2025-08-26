8cf3fc896fb7f234393106a7b97a16a4ab269b31
Gas7oon <gaston1992@gmail.com>
2025-08-25 23:57:02 -0300

    guia 9 ejer codificacion

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -5,17 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Producto;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProductoController extends AbstractController
 {
     #[Route('/', name: 'listar_productos')]
     public function listarProductos(): Response
- {
-   return $this->render('producto/lista.html.twig');
- }
+    {
+        // Renderiza la lista de productos (Twig)
+        return $this->render('producto/lista.html.twig');
+    }
 
+    #[Route('/product', name: 'create_product')]
+    public function crearProducto(EntityManagerInterface $entityManager): Response
+    {
+        // Creamos 10 productos
+        for ($i = 1; $i <= 10; $i++) {
+            $producto = new Producto();
+            $producto->setNombre('Producto '.$i);
+            $producto->setDescripcion('Lorem ipsum '.$i);
+            $producto->setPrecio(mt_rand(10, 100));
+            $producto->setImagen('images/producto'.$i.'.jpg');
 
+            // Persistimos cada producto con $entityManager
+            $entityManager->persist($producto);
+        }
 
+        // Guardamos todos los productos en la base
+        $entityManager->flush();
+
+        return new Response('Se han creado 10 productos correctamente.');
+    }
 }
-
-?>
---
+++ src/DataFixtures/ProductoFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Producto;
+
+class ProductoFixtures extends Fixture
+{
+ public function load(ObjectManager $manager): void
+{
+    for ($i = 1; $i <= 10; $i++) {
+        $producto = new Producto();
+        $producto->setNombre("Producto{$i}");
+        $producto->setDescripcion("Lorem ipsum para el producto {$i}");
+        $producto->setPrecio(rand(10, 100));
+        $producto->setImagen("images/producto{$i}.jpg");
+
+        $manager->persist($producto);
+    }
+
+    $manager->flush();
+}
+
+}
